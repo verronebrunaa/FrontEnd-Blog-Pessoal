d9343bff8c47b371de34b69b79dfcaa8ec9917cb
verronebruna <verronebruna@gmail.com>
2022-02-21 21:28:54 -0300

    Atualização Cadastro Usuario - atualizando erro

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import './CadastroUsuario.css';
 
 function CadastroUsuario(){
-    return (<h1></h1>);
+    return (<h1>Cadastro de Usuario</h1>);
 }
 export default CadastroUsuario;
