1c2e32b05a118a7e0ac7cb26d0a582e8905dc218
verronebruna <verronebruna@gmail.com>
2022-02-21 22:20:23 -0300

    Atividade de 21-02

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import {Link} from 'react-router-dom'
 import './CadastroUsuario.css';
 
-function CadastroUsuario(){
-    return (<h1>Cadastro de Usuario</h1>);
+function CadastroUsuario() {
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+                <Box paddingX={10}>
+                    <form>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='texto'>Cadastrar</Typography>
+                        <TextField id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/home' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' className='botao'>
+                                    Logar
+                                </Button>
+                            </Link>
+                        </Box>
+                    </form>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 }
 export default CadastroUsuario;
