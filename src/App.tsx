6cdcc3b8576366f988952d5c054560c802eb2969
verronebruna <verronebruna@gmail.com>
2022-02-25 00:29:57 -0300

    Atualização botoes navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Login from './pages/login/Login';
 import {Grid} from '@material-ui/core';
 import Home from './pages/home/Home';
 import './App.css';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listatema/ListaTema';
 
 function App() {
   return (
@@ -29,6 +31,14 @@ function App() {
 
           <Route path='/cadastrousuario'>
             <CadastroUsuario />
+          </Route>
+
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+
+          <Route path='/posts'>
+            <ListaPostagem />
           </Route>
         </div>
       </Switch>
--- src/components/estatics/navbar/navbar.tsx
+++ src/components/estatics/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Avatar } from '@material-ui/core';
+import { AppBar, Toolbar, Box, Avatar, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 function Navbar() {
@@ -10,38 +10,35 @@ function Navbar() {
 
                     <Box className="text-decorator-none" display="flex">
                         <Link to='/home' className='text-decorator-none' >
-                                <Box>
-                                    <Avatar alt="Logo: livro" src='https://cdn-icons.flaticon.com/png/512/4212/premium/4212480.png?token=exp=1645718162~hmac=8a9961bd9ff34a9249ef4be8658130c2' />
-                                </Box>
-                        </Link>
-
-                        <Box mx={1} className='cursor' >
-                            <Typography variant="h6" color="inherit">
-                                Posts
-                            </Typography>
-                        </Box>
-
-                        <Box mx={1} className='cursor' >
-                            <Typography variant="h6" color="inherit">
-                                Temas
-                            </Typography>
-                        </Box>
-
-                        <Box mx={1} className='cursor' >
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar Tema
-                            </Typography>
-                        </Box>
-
-                        <Link to='/login' className='text-decorator-none'>
-                            <Box mx={1} className='cursor' >
-                                <Typography variant="h6" color="inherit">
-                                    Logout
-                                </Typography>
+                            <Box>
+                                <Avatar alt="Logo: livro" src='https://cdn-icons.flaticon.com/png/512/4212/premium/4212480.png?token=exp=1645718162~hmac=8a9961bd9ff34a9249ef4be8658130c2' />
                             </Box>
                         </Link>
-                    </Box>
 
+                        <Box mx={1} display="flex">
+                            <Link to="/posts" className="text-decorator-none">
+                                <Button variant="outlined">Postagens</Button>
+                            </Link>
+                        </Box>
+
+                        <Box mx={1} display="flex">
+                            <Link to="/temas" className="text-decorator-none">
+                                <Button variant="outlined">Temas</Button>
+                            </Link>
+                        </Box>
+
+                        <Box mx={1} display="flex">
+                            <Link to="/cadastrotemas" className="text-decorator-none">
+                                <Button variant="outlined">Cadastrar Tema</Button>
+                            </Link>
+                        </Box>
+
+                        <Box mx={1} display="flex">
+                            <Link to="/login" className="text-decorator-none">
+                                <Button variant="outlined">Logout</Button>
+                            </Link>
+                        </Box>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </>
